d8f3e16bccbede4e4e98b90658b5845374f79751
salelibertas <salelibertas@gmail.com>
2022-07-01 17:42:32 +0200

    vue_komponente

---
+++ my-component.vue
@@ -0,0 +1,28 @@
+<!-- kalup -->
+<template>
+    <div class="user-container">
+        <h3>{{user.name}}</h3>
+        <p>{{user.email}}</p>
+        <p class="balance">{{user.balance}}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "MyComponent",
+    props: {
+        user: Object
+    }
+};
+</script>
+
+
+<style>
+    h3 {
+        text-decoration: underline;
+    }
+    .balance {
+        font-style: italic;
+    }
+</style>
